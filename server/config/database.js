c3f851d726b3a4fd4a251c83d5ce663ac2f21a47
Wahid Nurhisyam <ichamdaiboz@gmail.com>
2026-01-20 21:24:09 +0700

    chore: update database configuration and enhance .gitignore

--- server/config/database.js
+++ server/config/database.js
@@ -3,23 +3,23 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || "database_development",
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "githired_development",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres", // or 'mysql', 'sqlite', etc.
   },
   test: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || "database_test",
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "githired_test",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || "database_production",
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "githired_production",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres",
   },
