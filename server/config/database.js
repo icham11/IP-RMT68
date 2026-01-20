e7cc20ab90a8996bfe8c885a2c1656482e62c4ed
Wahid Nurhisyam <ichamdaiboz@gmail.com>
2026-01-20 21:10:39 +0700

    chore: add initial package.json for server with dependencies

---
+++ server/config/database.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "database_development",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres", // or 'mysql', 'sqlite', etc.
+  },
+  test: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "database_production",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres",
+  },
+};
